8c9843aecebf4ce1267717d5390ac2106d8bfb37
Tijana <tijana.ilic87@protonmail.com>
2020-07-27 23:19:52 +0200

    Style Payment component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,11 +4,15 @@ import StripeCheckout from "react-stripe-checkout";
 class Payments extends Component {
     render() {
       return (
-        <StripeCheckout 
+        <StripeCheckout
+          name="Feedback Collector"
+          description="$5 for 5 survey credits"
           amount={500}
           token={token => console.log(token)}
           stripeKey={process.env.REACT_APP_STRIPE_KEY}
-        />
+        >
+          <a className="btn red red-text lighten-5"><i className="material-icons left">add_circle_outline</i>Add Credits</a>
+        </StripeCheckout>
       );
     }
 }
