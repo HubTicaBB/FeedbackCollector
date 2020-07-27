6d5b9aced39b0fe323ddf0b58f7c26d224363f2d
Tijana <tijana.ilic87@protonmail.com>
2020-07-27 19:18:53 +0200

    Update Header content

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return(
+          <li><a href="/auth/google">Login with Google</a></li>
+        );
+      default:
+        return(
+          <li><a href="/api/logout">Logout</a></li>
+        )
+    }
+  }
+
   render() {
-    return(    
+    return (
       <nav>
         <div class="nav-wrapper">
           <a href="#" class="left brand-logo">&nbsp;Feedback Collector</a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="#">Login with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +31,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
